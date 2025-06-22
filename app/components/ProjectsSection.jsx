40728e8d711999114a4dfe567465b8a9e9411d45
jim-gyas <jackandthebean1234@gmail.com>
2025-06-22 10:31:02 -0400

    added resend api keys and my domain

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -14,25 +14,26 @@ const projectsData = [{
     previewUrl:"http://arowana.uk/"   
 },
 {
-    id:2,
-    title:"Dashboard React App",
-    description:"Developed a React-based dashboard with Syncfusion controls for enriched data visualization. Enhanced UI design for a more user-friendly experience. Collaborated with the team to seamlessly integrate advanced UI components.",
-    image:"./images/portfolio_pic.png",
-    tag: ["All"],
-    gitUrl:"https://github.com/jim-gyas/fundraising-insights-dashboard",
-    previewUrl:"https://github.com/jim-gyas/fundraising-insights-dashboard"
-},
-{
-  id:4,
-  title:"Donation Trend Analysis and Reporting for Nonprofit Fundraising  ",
-  description:"An end-to-end fundraising analytics project using Excel, Python, SQL, and Power BI. Analyzed real donation data made to nonprofits affiliated with elected officials. Built dashboards and models to track donation trends, regional insights, and organizational impact.",
+  id:2,
+  title:"Dashboard React App",
+  description:"Developed a React-based dashboard with Syncfusion controls for enriched data visualization. Enhanced UI design for a more user-friendly experience. Collaborated with the team to seamlessly integrate advanced UI components.",
   image:"./images/project2.png",
   tag: ["All","Web"],
   gitUrl:"https://github.com/jim-gyas/Dashboard-React-app",
   previewUrl:"https://tinshop.netlify.app/"
 },
 {
-    id:5,
+    id:3,
+    title:"Donation Trend Analysis and Reporting for Nonprofit Fundraising  ",
+    description:"An end-to-end fundraising analytics project using Excel, Python, SQL, and Power BI. Analyzed real donation data made to nonprofits affiliated with elected officials. Built dashboards and models to track donation trends, regional insights, and organizational impact.",
+    image:"./images/portfolio_pic.png",
+    tag: ["All"],
+    gitUrl:"https://github.com/jim-gyas/fundraising-insights-dashboard",
+    previewUrl:"https://github.com/jim-gyas/fundraising-insights-dashboard"
+},
+
+{
+    id:4,
     title:"Thread Next.js 13 App",
     description:"Developed a social networking app with Next.js, React, and Node.js, utilizing MongoDB for backend storage. Integrated Clerk for authentication and messaging, optimizing performance with shadcn. Enhanced functionality for a seamless user experience.",
     image:"./images/project3.png",
@@ -41,7 +42,7 @@ const projectsData = [{
     previewUrl:"https://github.com/jim-gyas/Thread-Application"
 },
 {
-    id:6,
+    id:5,
     title:"Sorting Visualizer",
     description:"Built a Sorting Visualizer in C++ with SDL2, featuring popular sorting algorithms like Insertion, Selection, Bubble, Merge, Quick, and Heap Sort. Provides a hands-on experience to understand and compare algorithm efficiency.",
     image:"./images/project4.png",
